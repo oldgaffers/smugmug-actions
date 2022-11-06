84618da8ce515a7af38817ac3615efdf3ad50ee6
jcable <julian.cable@bbc.co.uk>
2022-11-06 19:37:18 +0000

    fix: refactor

--- smugmug_create_album/handler.py
+++ smugmug_create_album/handler.py
@@ -3,9 +3,7 @@ import boto3
 from botocore.exceptions import ClientError
 from requests_oauthlib import OAuth1Session
 
-def get_secret():
-
-    secret_name = "boatregister/smugmug"
+def get_secret(secret_name):
     region_name = "eu-west-1"
     session = boto3.session.Session()
     client = session.client(
@@ -18,28 +16,32 @@ def get_secret():
         )
     except ClientError as e:
         raise e
-    return get_secret_value_response['SecretString']
+    return json.loads(get_secret_value_response['SecretString'])
 
-def lambda_handler(event, context):
-    print(event)
-    secrets = get_secret()
-    smugmug = OAuth1Session(secrets['api-key'],
+def getRequestsHandler():
+    secrets = get_secret("boatregister/smugmug")
+    return OAuth1Session(secrets['api-key'],
         client_secret=secrets['api-key-secret'],
         resource_owner_key=secrets['access_token'],
         resource_owner_secret=secrets['access_token_secret']
     )
-    data = { 'UrlName': 'NCC-1701', 'Name': 'Enterprise (NCC-1701)' }
+
+def createAlbum(smugmug, name, oga_no):
     r = smugmug.post(
         'https://www.smugmug.com/api/v2/folder/user/oga/Boats!albums',
-        json.dumps(data),
+        json.dumps({ 'UrlName': f'OGA-{oga_no}', 'Name': f'{name} ({oga_no})' }),
         headers={'Accept':'application/json', 'Content-Type': 'application/json'}
     )
     print(r.status_code)
     print(r.text)
     print(r.json())
-};
 
-
+def lambda_handler(event, context):
+    print(event)
+    smugmug = getRequestsHandler()
+    name = ''
+    oga_no = ''
+    createAlbum(smugmug, name, oga_no)
     # TODO implement
     return {
         'statusCode': 200,
