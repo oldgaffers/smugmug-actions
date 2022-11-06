1a0baca9000560408dee330397817e8c5e17fab5
jcable <julian.cable@bbc.co.uk>
2022-11-06 13:41:57 +0000

    fix: secret

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,27 @@
 import json
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "boatregister/smugmug"
+    region_name = "eu-west-1"
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+    return get_secret_value_response['SecretString']
 
 def lambda_handler(event, context):
     print(event)
+    print(get_secret())
     # TODO implement
     return {
         'statusCode': 200,
