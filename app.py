720457dc89abc97ebaec3b3992b818d189816d6e
jcable <julian.cable@bbc.co.uk>
2022-11-06 16:34:35 +0000

    fix: wip

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from aws_cdk.cloudformation_include import CfnInclude
 
 env = Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
-stack_name = 'mystack'
+stack_name = 'smugmug-create-album'
 app = App()
 stack = Stack(app, 'wrapper-stack', env=env, stack_name=stack_name)
 CfnInclude(stack, 'included-template', template_file=os.path.join(os.getcwd(), 'cfn', 'smugmug-create-album.yaml'))
