6f025d2c15ee9dff463af52bbfd79f6049fb5823
jcable <boatregister@oga.org.uk>
2025-10-26 15:38:43 +0000

    fix: uploads

--- smugmug_actions/handler.py
+++ smugmug_actions/handler.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-from urllib.parse import unquote
+from urllib.parse import unquote_plus
 from smugmug_actions.album import createAlbum, getAlbumKey
 from smugmug_actions.image import thumbnail, image
 from smugmug_actions.upload import uploadToSmugMug
@@ -12,14 +12,22 @@ def upload(bucket, key):
     # print('upload', bucket, key)
     o = s3.get_object(Bucket=bucket, Key=key)
     email, filename = key.split('/')
-    url, copyright = uploadToSmugMug(filename, o)
-    send_email(url, email, copyright)
+    meta = o['Metadata']
+    if 'albumkey' in meta:
+        albumkey = meta.get('albumkey', None)
+        copyright = meta.get('copyright', 'OGA')
+        url = uploadToSmugMug(filename, albumkey, copyright, o['Body'])
+        send_email(url, email, copyright)
+        return
+    if 'uuid' in meta:
+        print('TODO, local S3 copy', meta)
+    print('missing album key')
 
 def lambda_handler(event, context):
     # print(json.dumps(event))
     if 'Records' in event:
         s3 = event['Records'][0]['s3']
-        return upload(s3['bucket']['name'], unquote(s3['object']['key']))
+        return upload(s3['bucket']['name'], unquote_plus(s3['object']['key']))
     if event['requestContext']['http']['method'] == 'GET':
         qsp = event['queryStringParameters']
         path = event['requestContext']['http']['path']
--- smugmug_actions/upload.py
+++ smugmug_actions/upload.py
@@ -3,26 +3,20 @@ from smugmug_actions.auth import getRequestsHandler
 
 ssm = boto3.client('ssm')
 
-def uploadToSmugMug(filename, o):
+def uploadToSmugMug(filename, albumkey, copyright, body):
     smugmug = getRequestsHandler()
-    meta = o['Metadata']
-    if 'albumkey' not in meta:
-      print('missing album key')
-      return
-    copyright = meta.get('copyright', 'OGA')
     headers={
             'Content-Type': o['ContentType'],
             'Content-Length': f"{o['ContentLength']}",
           # 'Content-MD5': base64.b64encode(hashlib.md5(data).digest()),
-           'X-Smug-AlbumUri': f"/api/v2/album/{meta['albumkey']}",
+           'X-Smug-AlbumUri': f"/api/v2/album/{albumkey}",
            'X-Smug-FileName': filename,
            'X-Smug-Caption': f"© {copyright}",
            'X-Smug-Hidden': 'true',
            'X-Smug-ResponseType': 'JSON',
            'X-Smug-Version': 'v2'
         }
-    # print('upload', key, o['Metadata']['albumkey'])
-    r=smugmug.post('https://upload.smugmug.com/', headers=headers, data=o['Body'].read())
+    # print('upload', key, albumkey)
+    r=smugmug.post('https://upload.smugmug.com/', headers=headers, data=body.read())
     if r.ok:
-        return r.json()['Image']['URL'], copyright
-    return None, copyright
+        return r.json()['Image']['URL']
