550d59b451bdaaf085a4fd554d11cadc5b359d25
jcable <boatregister@oga.org.uk>
2025-11-27 12:55:46 +0000

    fix: args

--- smugmug_actions/upload.py
+++ smugmug_actions/upload.py
@@ -3,11 +3,11 @@ from smugmug_actions.auth import getRequestsHandler
 
 ssm = boto3.client('ssm')
 
-def uploadToSmugMug(filename, albumkey, copyright, body):
+def uploadToSmugMug(filename, albumkey, copyright, body, contentType, contentLength):
     smugmug = getRequestsHandler()
     headers={
-            'Content-Type': o['ContentType'],
-            'Content-Length': f"{o['ContentLength']}",
+            'Content-Type': contentType,
+            'Content-Length': f"{contentLength}",
           # 'Content-MD5': base64.b64encode(hashlib.md5(data).digest()),
            'X-Smug-AlbumUri': f"/api/v2/album/{albumkey}",
            'X-Smug-FileName': filename,
